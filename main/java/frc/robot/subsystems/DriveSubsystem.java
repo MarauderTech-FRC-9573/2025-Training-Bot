8818455a99d6bec9d47f16fa6242c2efefc3edc8
EvaSerra <eserrasierra@gmail.com>
2025-11-17 16:06:15 -0800

    Revert "Made subsystems and some commands"
    This reverts commit 1edefbfcfccc92d75e1d12938fd5946aa2b50d38.

--- main/java/frc/robot/subsystems/DriveSubsystem.java
+++ main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -11,42 +11,16 @@ public class DriveSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
 
   // TODO: Initialize a DifferentialDrive variable
-  private final DifferentialDrive differentialDrive;
   // TODO: Initialize four CANSparkMax motors
-  private final CANSparkMax frontLeftMotor;
-  private final CANSparkMax frontRightMotor;
-  private final CANSparkMax backLeftMotor;
-  private final CANSparkMax backRightMotor;
-
   public ExampleSubsystem() {
     // TODO: Set motor current limits
-    frontLeftMotor = new CANSparkMax(2, MotorType.kBrushless);
-    frontRightMotor = new CANSparkMax(3, MotorType.kBrushless);
-    backLeftMotor = new CANSparkMax(4, MotorType.kBrushless);
-    backRightMotor = new CANSparkMax(5, MotorType.kBrushless);
-
-    SparkMaxConfig config = new SparkMaxConfig();
-    config.smartCurrentLimit(40);
-    frontLeftMotor.configure(config, null, null);
-    frontRightMotor.configure(config, null, null);
-    backLeftMotor.configure(config, null, null);
-    backRightMotor.configure(config, null, null);
-
     // TODO: Set the rear left and right motors to be followers
-    backLeftMotor.follow(frontLeftMotor);
-    backRightMotor.follow(frontRightMotor);
     // TODO: Set one side of the motors to be inverted 
-    frontRightMotor.setInverted(true);
     // TODO: Assign the DifferentialDrive variable to a new DifferentialDrive object passing in the two front motors
-    differentialDrive = new DifferentialDrive(frontLeftMotor, frontRightMotor);
     // TODO: Pass in a max speed 
-    differentialDrive.setMaxOutput(0.8);
   }
 
   // TODO: Create an arcadeDrive method, taking in speed and rotation 
-  public void arcadeDrive(double speed, double rotation) {
-    differentialDrive.arcadeDrive(speed, rotation);
-  }
 
   /**
    * Example command factory method.
